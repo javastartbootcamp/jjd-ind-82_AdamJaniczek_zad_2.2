037c252f7c1d56a9b241a8d3e55890e988c5886f
AdamJaniczek <152891309+AdamJaniczek@users.noreply.github.com>
2023-12-06 19:52:12 +0100

    Rozwiązanie zadania

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,16 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
+        String firstProductName = "Mleko";
+        String firstNameOfManufacturer = "Mlekowita";
+        double firstProductPrice = 2.50;
+        String secondProductName = "Czekolada";
+        String secondNameOfManufacturer = "Wedel";
+        double secondProductPrice = 2.19;
+        System.out.printf("%s %s %.2f", firstProductName, firstNameOfManufacturer, firstProductPrice);
+        System.out.println();
+        System.out.printf("%s %s %.2f", secondProductName, secondNameOfManufacturer, secondProductPrice);
+
 
     }
 }
